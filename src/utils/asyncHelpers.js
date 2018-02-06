5e5d234e0c28cd1d2f682acf2511355fce588b7e
Anton <anton@avalanchelabs.ee>
2018-02-06 11:09:33 +0200

    add reselect + normalizr

--- src/utils/asyncHelpers.js
+++ src/utils/asyncHelpers.js
@@ -1,5 +1,7 @@
 import { createAction } from 'redux-actions';
 import _ from 'lodash';
+import { denormalize } from 'normalizr';
+import { createSelector } from 'reselect';
 
 const toSuccess = asyncType => `${asyncType}_SUCCESS`;
 const toFail = asyncType => `${asyncType}_FAIL`;
@@ -43,5 +45,31 @@ const createAsyncReducer = asyncType => ({
     error: payload,
   }),
 });
+
+const createAsyncNormalizeSelector = (schema, selector) => {
+  const isSchemaArray = _.isArray(schema);
+  const schemaSingle = isSchemaArray ? schema[0] : schema;
+
+  return createSelector(
+    [
+      selector,
+      state => state.entities,
+      state => state.entities[schemaSingle.key],
+    ],
+    (listOrId, allList, allEntities) => {
+      const dataListOrId = _.get(listOrId, 'data');
+
+      if (_.isEmpty(dataListOrId)) {
+        return isSchemaArray ? [] : {};
+      }
+
+      const data = isSchemaArray
+        ? dataListOrId.map(id => allList.get(id.toString()))
+        : _.get(allList, dataListOrId.toString());
+
+      return schema ? denormalize(data, schema, allEntities) : data;
+    }
+  );
+};
 
 export { createAsyncAction, initialAsyncState, createAsyncReducer };
